3d4574669bc2ca230b13e89be6e4c58985114aba
Akhil <24322269+LaraAkhil@users.noreply.github.com>
2019-12-06 21:33:25 +0530

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Article } from "./article/article.model";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'angular-reddit';
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article("google", "http://www.google.com", 20),
+      new Article("bing", "http://www.bing.com", 10),
+      new Article("reddit", "http://www.reddit.com", 30)
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value));
+    return false;
+  }
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
 }
